8cf3fd9d545735636ed113dad7392afc3903239c
Mukesh Sharma <msharma@calance.com>
2018-11-29 09:37:50 -0400

    updated travis yaml

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ cd $PWD/eks/terraform
 
 echo $PWD
 
-docker run --name terraform -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -i -t hashicorp/terraform:light validate
-docker run --name terraform -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -i -t hashicorp/terraform:light plan
-docker run --name terraform -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -i -t hashicorp/terraform:light apply -auto-approve
+docker run --name terraform -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -i -t hashicorp/terraform:light validate $PWD
+docker run --name terraform -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -i -t hashicorp/terraform:light plan $PWD
+docker run --name terraform -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -i -t hashicorp/terraform:light apply -auto-approve $PWD
