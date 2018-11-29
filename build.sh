c96de7e33fde82b740bc1743054939c68c77f16b
Mukesh Sharma <msharma@calance.com>
2018-11-29 09:55:11 -0400

    updated travis yaml

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-docker run --name terraform -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
+docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
    --mount type=bind,source=$PWD/,target=/tmp \
    -i -t hashicorp/terraform:light init /tmp/eks/terraform
-docker run --name terraform -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
+docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
    --mount type=bind,source=$PWD/,target=/tmp \
    -i -t hashicorp/terraform:light validate /tmp/eks/terraform
-docker run --name terraform -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
+docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
     --mount type=bind,source=$PWD/,target=/tmp \
     -i -t hashicorp/terraform:light plan /tmp/eks/terraform
-docker run --name terraform -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
+docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY \
     --mount type=bind,source=$PWD/,target=/tmp \
     -i -t hashicorp/terraform:light apply -auto-approve /tmp/eks/terraform
