4316a6e2cc64d11965b23c907a8cbd94b612217f
Mukesh Sharma <msharma@calance.com>
2018-11-29 09:51:57 -0400

    updated travis yaml

--- eks/terraform/terraform.tf
+++ eks/terraform/terraform.tf
@@ -1,11 +1,10 @@
-data "aws_caller_identity" "current" {}
 
 terraform {
     backend "s3" {
         encrypt = true
-        bucket = "${data.aws_caller_identity.current.account_id}-tf-state"
-        dynamodb_table = "terraform-state-lock-dynamo-${data.aws_caller_identity.current.account_id}"
+        bucket = "753182529116-tf-state"
+        dynamodb_table = "terraform-state-lock-dynamo-753182529116"
         region = "us-east-1"
-        key = "${data.aws_caller_identity.current.account_id}/"
+        key = "753182529116/"
     }
 }
