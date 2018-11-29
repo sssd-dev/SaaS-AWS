7a7a1cfb58652601c01718a12bd42391165ef153
Mukesh Sharma <msharma@calance.com>
2018-11-29 11:07:23 -0400

    updated travis yaml

--- eks/terraform/variables.tf
+++ eks/terraform/variables.tf
@@ -6,12 +6,24 @@ variable "my_pub_ip" {
   type = "string"
   default = "156.34.161.141/32"
 }
+variable "vpc_id" {
+  default = "vpc-59d89921"
+}
 
 data "aws_vpc" "default" {
+  id = "${var.vpc_id}"
 }
 data "aws_region" "current" {}
 
-data "aws_subnet" "default" {}
+data "aws_subnet_ids" "default" {
+  id = "${var.vpc_id}"
+}
+
+data "aws_subnet" "default" {
+  id = "${data.aws_subnet_ids.default.ids[count.index]}"
+  count = "${length(data.aws_subnet_ids.default.ids)}"
+}
+
 data "aws_ami" "eks-worker" {
   filter {
     name   = "name"
