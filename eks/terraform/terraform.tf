03d519fa0f5b5bb73add866eabb9dc4672df77ee
Mukesh Sharma <mukesh.sharma@intelerad.com>
2018-12-12 19:22:53 -0400

    Updating kubectl and awscli installation

--- eks/terraform/terraform.tf
+++ eks/terraform/terraform.tf
@@ -1,10 +1,10 @@
+# terraform {
+#     backend "s3" {
+#         encrypt = true
+#         bucket = "753182529116-tf-state"
+#         dynamodb_table = "terraform-state-lock-dynamo-753182529116"
+#         region = "us-east-1"
+#         key = "753182529116/"
+#     }
+# }
 
-terraform {
-    backend "s3" {
-        encrypt = true
-        bucket = "753182529116-tf-state"
-        dynamodb_table = "terraform-state-lock-dynamo-753182529116"
-        region = "us-east-1"
-        key = "753182529116/"
-    }
-}
